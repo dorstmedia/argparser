989da5847ee5db89b3e1b3e74c43c3148e37cf68
dorstmedia <86899017+dorstmedia@users.noreply.github.com>
2024-08-11 19:51:40 +0200

    Update bash_history.sh

--- bash_history/usr/local/lib/bash_history/bash_history.sh
+++ bash_history/usr/local/lib/bash_history/bash_history.sh
@@ -1,13 +1,34 @@
 #!/bin/bash
+hc_src_err=false; src_file_name="";
 
-# set either to "hcmnt" for full version 
-# or "hcmnts" for compact version
-hcmnt_fn="hcmnt" # hcmnt_fn="hcmnts" 
-# set a default (must use -e option to include it)
+# set variant to "hcmnt" for full version 
+hc_variant="hcmnt" 
 
-# source selected version of function and start using it
-fn_hcmnt_file="$(dirname "${BASH_SOURCE[0]}")/fn.${bash_history_fn}.sh"
-[ -f "$fn_hcmnt_file" ] && source "$fn_hcmnt_file" && {
-  export hcmntextra='date "+%Y%m%d %R"' \
-  export PROMPT_COMMAND="${hcmnt_fn}"
-}
+# set variant to "hcmnts" for compact version 
+# hc_variant="hcmnts" 
+
+# set variant to "hcmin" for minimal version 
+# hc_variant="hcmin" 
+
+if [ "$hc_variant" == "hcmnt" ] || [ "$hc_variant" == "hcmnt" ]; then src_file_name="fn.${hc_variant}.sh"
+elif [ "$hc_variant" == "hcmin" ]; then src_file_name="cfg.${hc_variant}.sh"; fi
+if [ "$src_file_name" != "" ]; then
+  # set source file for function or lib
+  src_file="$(dirname "${BASH_SOURCE[0]}")/${src_file_name}"
+  # check if src file exists
+  if [ -f "$src_file" ]; then
+    # source selected version of function and start using it
+    source "$src_file" || hc_src_err=true
+    # check if src file was sourced successfully
+    if [ "$hc_src_err" == false ]; then
+      if [ "$hc_variant" == "hcmnt" ] || [ "$hc_variant" == "hcmnt" ]; then 
+          # set hcmnt/hcmnts variant specific stuff
+          export hcmntextra='date "+%Y%m%d %R"' \
+          export PROMPT_COMMAND="${hc_variant}"
+      fi
+    fi
+  fi
+fi
+  
+
+  
