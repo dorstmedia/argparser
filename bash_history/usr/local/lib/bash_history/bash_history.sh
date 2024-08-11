beb7fdff7d10416fe371e6b0f5191b476a9ba2b1
dorstmedia <86899017+dorstmedia@users.noreply.github.com>
2024-08-11 19:58:47 +0200

    Update bash_history.sh

--- bash_history/usr/local/lib/bash_history/bash_history.sh
+++ bash_history/usr/local/lib/bash_history/bash_history.sh
@@ -7,28 +7,40 @@ hc_variant="hcmnt"
 # set variant to "hcmnts" for compact version 
 # hc_variant="hcmnts" 
 
-# set variant to "hcmin" for minimal version 
-# hc_variant="hcmin" 
+# set variant to "lite" for extended minimal version 
+# hc_variant="lite" 
 
-if [ "$hc_variant" == "hcmnt" ] || [ "$hc_variant" == "hcmnt" ]; then src_file_name="fn.${hc_variant}.sh"
-elif [ "$hc_variant" == "hcmin" ]; then src_file_name="cfg.${hc_variant}.sh"; fi
-if [ "$src_file_name" != "" ]; then
-  # set source file for function or lib
-  src_file="$(dirname "${BASH_SOURCE[0]}")/${src_file_name}"
-  # check if src file exists
-  if [ -f "$src_file" ]; then
-    # source selected version of function and start using it
-    source "$src_file" || hc_src_err=true
-    # check if src file was sourced successfully
-    if [ "$hc_src_err" == false ]; then
-      if [ "$hc_variant" == "hcmnt" ] || [ "$hc_variant" == "hcmnt" ]; then 
-          # set hcmnt/hcmnts variant specific stuff
-          export hcmntextra='date "+%Y%m%d %R"' \
-          export PROMPT_COMMAND="${hc_variant}"
+# set variant to "min" for minimal version 
+# hc_variant="min" 
+
+if [ "$hc_variant" == "hcmnt" ] || [ "$hc_variant" == "hcmnt" ]; then 
+  src_file_name="fn.${hc_variant}.sh";
+  if [ "$src_file_name" != "" ]; then
+    # set source file for function or lib
+    src_file="$(dirname "${BASH_SOURCE[0]}")/${src_file_name}"
+    # check if src file exists
+    if [ -f "$src_file" ]; then
+      # source selected version of function and start using it
+      source "$src_file" || hc_src_err=true
+      # check if src file was sourced successfully
+      if [ "$hc_src_err" == false ]; then
+        export hcmntextra='date "+%Y%m%d %R"' \
+        export PROMPT_COMMAND="${hc_variant}"        
       fi
     fi
   fi
+elif [ "$hc_variant" == "lite" ] || [ "$hc_variant" == "min" ]; then
+    export HISTCONTROL=ignoredups:erasedups  # no duplicate entries
+    export HISTSIZE=100000                   # big big history
+    export HISTFILESIZE=100000               # big big history
+    # When the shell exits, append to the history file instead of overwriting it
+    shopt -s histappend                      # append to history, don't overwrite it
+    if [ "$hc_variant" == "lite" ]; then
+      # After each command, append to the history file and reread it
+      PROMPT_COMMAND="${PROMPT_COMMAND:+$PROMPT_COMMAND$'\n'}history -a; history -c; history -r"
+    elif [ "$hc_variant" == "min" ]; then
+      # Save and reload the history after each command finishes
+      export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
+    fi
+  fi
 fi
-  
-
-  
